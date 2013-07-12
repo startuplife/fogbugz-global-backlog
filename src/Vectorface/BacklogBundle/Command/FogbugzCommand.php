a9160130eeb5e7bea0f80cdf6680755359fc91de
ckdarby <ckdarby@gmail.com>
2013-07-12 12:00:25 -0400

    Adding the close fogbugz sync'ing (Kindof hax)

--- src/Vectorface/BacklogBundle/Command/FogbugzCommand.php
+++ src/Vectorface/BacklogBundle/Command/FogbugzCommand.php
@@ -25,13 +25,20 @@ class FogbugzCommand extends BacklogBundle\AbstractCommand
     protected function configure()
     {
         $this
-            ->setName('backlog:fogbugz:populate')
-            ->setDescription('Backlog Fogbugz')
-            ->addArgument(
-                'range',
-                InputArgument::OPTIONAL,
-                'What range are we populating the missing data from?'
-            );
+        ->setName('backlog:fogbugz')
+        ->setDescription('Backlog Fogbugz')
+        ->addOption(
+           'populate',
+           null,
+           InputOption::VALUE_OPTIONAL,
+           'Will fetch all open tickets'
+        )
+        ->addOption(
+           'closed',
+           null,
+           InputOption::VALUE_OPTIONAL,
+           'Will remove backlogs that are closed in Fogbugz'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -42,17 +49,17 @@ class FogbugzCommand extends BacklogBundle\AbstractCommand
             $container->getParameter('fogbugz_user'),
             $container->getParameter('fogbugz_password'),
             $container->getParameter('fogbugz_url')
-        );
+            );
 
         $this->fogbugz->logon();
-        $totalTickets = $this->populate();
-        $output->writeln('Total tickets: <info>'.$totalTickets.'</info>');
+        $this->populate();
+        $this->removeClosedTickets();
     }
 
     public function populate()
     {
-        $xml = $this->fogbugz->search(array('q' => 'status:"open"', 'cols' => 'ixBug,sCategory,sTitle,sProject,ixProject,ixFixFor,sFixFor,sEmailAssignedTo,sPersonAssignedTo,ixPersonAssignedTo'));
-
+        $xml = $this->fogbugz->search(array('q' => 'status:"active" OR status:"open"', 'cols' => 'ixBug,sCategory,sTitle,sProject,ixProject,ixFixFor,sFixFor,sEmailAssignedTo,sPersonAssignedTo,ixPersonAssignedTo'));
+        $this->redis->del('VectorfaceBacklog:tickets');
         foreach($xml->children() as $tickets){
             foreach($tickets->children() as $ticket){
                 $data = array(
@@ -72,6 +79,19 @@ class FogbugzCommand extends BacklogBundle\AbstractCommand
             }
         }
         return $xml->cases->attributes()->count;
+    }
+
+    public function removeClosedTickets()
+    {
+        $this->redis->zInter('VectorfaceBacklog:listOfBacklogs', array('VectorfaceBacklog:tickets', 'VectorfaceBacklog:listOfBacklogs'), array(1, 0));
+        $backLogs = $this->redis->lRange("VectorfaceBacklog:rankOfBacklogs", 0, -1);
+        foreach($backLogs as $backlog) {
+            $exists = $this->redis->ZRANGEBYSCORE('VectorfaceBacklog:listOfBacklogs', $backlog, $backlog);
+            if(empty($exists)) {
+                $this->redis->lRem('VectorfaceBacklog:rankOfBacklogs', $backlog);
+            }
+
+        }
     }
 
 }
